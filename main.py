cdbeac8186f10e8f81df31dcd572055cfc8b956e
taypaycode <geometric.payton@gmail.com>
2025-09-28 22:02:18 -0400

    fixed the book reading script in main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+def main():
+    book_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+main()
