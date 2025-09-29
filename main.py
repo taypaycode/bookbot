de426b0729e2b7963db678116cb8d3fb5c26dc60
taypaycode <geometric.payton@gmail.com>
2025-09-29 13:20:39 -0400

    D: created working bookbot assignment based on AI assistance.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        return f.read()
-
-def word_count(text):
-    words = text.split()
-
-    return len(words)
+from stats import get_book_text, word_count, char_count, sort_output
 
 
 def main():
     book_path = "./books/frankenstein.txt"
+    
     book_text = get_book_text(book_path)
-
+    
     total_words = word_count(book_text)
 
+    characters = char_count(book_text)
+    
+    report_list = sort_output(characters)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
     print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    
+    for item in report_list:
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,10 +1,33 @@
-def get book_text(path_to_file):
+def get_book_text(path_to_file):
+    """reads the entire book from a file path"""    
     with open(path_to_file) as f:
         return f.read()
-
+    
 def word_count(text):
+    """Returns the number of words in the given text"""
     words = text.split()
-
     return len(words)
 
+def char_count(text):
+    """returns a dict of all chars lowercased and their counts"""
+    char_dict = {}
+    for char in text.lower():
+        if char in char_dict:
+            char_dict[char] +=1
+    
+        else: 
+            char_dict[char] = 1
+    return char_dict
 
+def sort_output(char_dict):
+    """convert char_dict to sorted list of dicts with 'char' and 'num' keys"""
+    report_list = []
+
+    for char, count in char_dict.items():
+        if char.isalpha():
+            report_list.append({"char": char, "num": count})
+
+
+    report_list.sort(key=lambda item:item["num"], reverse=True)
+
+    return report_list
